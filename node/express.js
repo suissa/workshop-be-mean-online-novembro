85025cde6dce2298e8e0cbd9c4fcd2faf36a81eb
Suissa <jnascimento@gmail.com>
2014-11-26 20:30:23 -0200

    Começando com o express

---
+++ node/express.js
@@ -0,0 +1,11 @@
+var express = require('express')
+  , app = express()
+  ;
+
+app.get('/hello', function(req, res){
+  var body = 'Hello World';
+  res.setHeader('Content-Type', 'text/plain');
+  res.setHeader('Content-Length', body.length);
+  res.end(body);
+});
+app.listen(3000);
