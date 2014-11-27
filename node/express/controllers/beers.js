5965c61c1ffe0f1aef70afca9d87733fea8bca1e
Suissa <jnascimento@gmail.com>
2014-11-26 22:16:41 -0200

    API de beers com find e findOne

--- node/express/controllers/beers.js
+++ node/express/controllers/beers.js
@@ -43,6 +43,22 @@ module.exports = {
     });
 
   }
+  , findOne: function (req, res) {
+
+    var query = {_id: req.params.id};
+
+    Beer.findOne(query, function (err, data) {
+      if (err) {
+        console.log('Erro: ', err);
+        msg = 'Erro: ' + err;
+      } else {
+        console.log('Listagem: ', data);
+        msg = data;
+      }
+      res.json(msg);
+    });
+
+  }
   , update: function (req, res) {
 
     var query = {name: /skol/i}
--- node/express/routes/api/beers.js
+++ node/express/routes/api/beers.js
@@ -1,10 +1,14 @@
 var express = require('express')
   , router = express.Router()
-  , Controller = require('../controllers/beers')
+  , Controller = require('../../controllers/beers')
   ;
 
 router.get('/', function(req, res) {
   Controller.retrieve(req, res);
+});
+
+router.get('/:id', function(req, res) {
+  Controller.findOne(req, res);
 });
 
 module.exports = router;
